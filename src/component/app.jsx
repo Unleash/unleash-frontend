fba19f9edbea878acf7e37c5600338ccf3ef2f22
Fredrik Oseberg <fredrik.no@gmail.com>
2021-02-26 12:29:03 +0100

    feat: add theme

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -1,20 +1,23 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Route, Redirect, Switch } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core";
 
-import Features from '../page/features';
-import { routes } from './menu/routes';
-import styles from './styles.module.scss';
-import AuthenticationContainer from './user/authentication-container';
-import MainLayout from './layout/main';
+import Features from "../page/features";
+import AuthenticationContainer from "./user/authentication-container";
+import MainLayout from "./layout/main";
 
+import mainTheme from "../themes/main-theme";
+import { routes } from "./menu/routes";
+
+import styles from "./styles.module.scss";
 class App extends PureComponent {
     static propTypes = {
         location: PropTypes.object.isRequired,
         match: PropTypes.object.isRequired,
         history: PropTypes.object.isRequired,
-        user: PropTypes.object,
+        user: PropTypes.object
     };
 
     render() {
@@ -22,22 +25,37 @@ class App extends PureComponent {
             return <AuthenticationContainer history={this.props.history} />;
         }
         return (
-            <div className={styles.container}>
-                <MainLayout {...this.props}>
-                    <Switch>
-                        <Route exact path="/" render={() => <Redirect to="/features" component={Features} />} />
-                        {routes.map(route => (
-                            <Route key={route.path} path={route.path} component={route.component} />
-                        ))}
-                    </Switch>
-                </MainLayout>
-            </div>
+            <ThemeProvider theme={mainTheme}>
+                <div className={styles.container}>
+                    <MainLayout {...this.props}>
+                        <Switch>
+                            <Route
+                                exact
+                                path="/"
+                                render={() => (
+                                    <Redirect
+                                        to="/features"
+                                        component={Features}
+                                    />
+                                )}
+                            />
+                            {routes.map(route => (
+                                <Route
+                                    key={route.path}
+                                    path={route.path}
+                                    component={route.component}
+                                />
+                            ))}
+                        </Switch>
+                    </MainLayout>
+                </div>
+            </ThemeProvider>
         );
     }
 }
 
 const mapStateToProps = state => ({
-    user: state.user.toJS(),
+    user: state.user.toJS()
 });
 
 export default connect(mapStateToProps)(App);
---
+++ src/themes/main-theme.js
@@ -0,0 +1,16 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#009688",
+            light: "#B2DFDB",
+            dark: "#00796B"
+        },
+        secondary: {
+            main: "#00BCD4"
+        }
+    }
+});
+
+export default theme;
