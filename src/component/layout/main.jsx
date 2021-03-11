17db4bfc10c832bdc2bb8ab3d312a2471e1ccb04
Fredrik Oseberg <fredrik.no@gmail.com>
2021-03-11 15:01:06 +0100

    refactor: layout

--- src/component/layout/main.jsx
+++ src/component/layout/main.jsx
@@ -1,36 +1,53 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import { Layout, Content, Footer, Grid, Cell } from 'react-mdl';
+import React from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import { makeStyles } from "@material-ui/styles";
+import { Grid } from "@material-ui/core";
 
-import styles from '../styles.module.scss';
-import ErrorContainer from '../error/error-container';
-import Header from '../menu/header';
-import ShowApiDetailsContainer from '../api/show-api-details-container';
-import { FooterMenu } from '../menu/footer';
+import styles from "../styles.module.scss";
+import ErrorContainer from "../error/error-container";
+import Header from "../menu/header";
+import ShowApiDetailsContainer from "../api/show-api-details-container";
+import { FooterMenu } from "../menu/footer";
 
-export default class App extends PureComponent {
-    static propTypes = {
-        location: PropTypes.object.isRequired,
-    };
-
-    render() {
-        return (
-            <Layout fixedHeader>
-                <Header location={this.props.location} />
-                <Content className={classnames('mdl-color--grey-50', styles.contentWrapper)}>
-                    <Grid noSpacing className={styles.content}>
-                        <Cell col={12}>
-                            <div className={styles.contentContainer}>{this.props.children}</div>
-                            <ErrorContainer />
-                        </Cell>
-                    </Grid>
-                    <Footer size="mega">
-                        <FooterMenu />
-                        <ShowApiDetailsContainer />
-                    </Footer>
-                </Content>
-            </Layout>
-        );
+const useStyles = makeStyles(theme => ({
+    footer: {
+        background: theme.palette.neutral.main,
+        padding: "2rem",
+        color: "#fff"
     }
-}
+}));
+
+const Layout = ({ children, location }) => {
+    const muiStyles = useStyles();
+
+    return (
+        <Grid container fixedHeader>
+            <Header location={location} />
+            <div
+                style={{ margin: "0 auto" }}
+                className={classnames(
+                    "mdl-color--grey-50",
+                    styles.contentWrapper
+                )}
+            >
+                <Grid item className={styles.content} xs={12} sm={12}>
+                    <div className={styles.contentContainer}>{children}</div>
+                    <ErrorContainer />
+                </Grid>
+            </div>
+            <Grid item xs={12} sm={12}>
+                <div className={muiStyles.footer}>
+                    <FooterMenu />
+                    <ShowApiDetailsContainer />
+                </div>
+            </Grid>
+        </Grid>
+    );
+};
+
+Layout.propTypes = {
+    location: PropTypes.object.isRequired
+};
+
+export default Layout;
