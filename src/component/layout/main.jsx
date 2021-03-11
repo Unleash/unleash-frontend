2ca773a45911c9b5e9bcccb88de53424ab1b6dad
Fredrik Oseberg <fredrik.no@gmail.com>
2021-03-11 16:50:54 +0100

    refactor: layout 100% min height

--- src/component/layout/main.jsx
+++ src/component/layout/main.jsx
@@ -14,7 +14,12 @@ const useStyles = makeStyles(theme => ({
     footer: {
         background: theme.palette.neutral.main,
         padding: "2rem",
-        color: "#fff"
+        color: "#fff",
+        height: "450px"
+    },
+    container: {
+        height: "100%",
+        justifyContent: "space-between"
     }
 }));
 
@@ -22,27 +27,23 @@ const Layout = ({ children, location }) => {
     const muiStyles = useStyles();
 
     return (
-        <Grid container fixedHeader>
+        <>
             <Header location={location} />
-            <div
-                style={{ margin: "0 auto" }}
-                className={classnames(
-                    "mdl-color--grey-50",
-                    styles.contentWrapper
-                )}
-            >
-                <Grid item className={styles.content} xs={12} sm={12}>
-                    <div className={styles.contentContainer}>{children}</div>
-                    <ErrorContainer />
-                </Grid>
-            </div>
-            <Grid item xs={12} sm={12}>
-                <div className={muiStyles.footer}>
+            <Grid container className={muiStyles.container}>
+                <div className={classnames(styles.contentWrapper)}>
+                    <Grid item className={styles.content} xs={12} sm={12}>
+                        <div className={styles.contentContainer}>
+                            {children}
+                        </div>
+                        <ErrorContainer />
+                    </Grid>
+                </div>
+                <div className={muiStyles.footer} style={{ width: "100%" }}>
                     <FooterMenu />
                     <ShowApiDetailsContainer />
                 </div>
             </Grid>
-        </Grid>
+        </>
     );
 };
 
