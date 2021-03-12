5fdfcaa56aa1df8714c40f9f824c76acd3a3667a
Fredrik Oseberg <fredrik.no@gmail.com>
2021-03-12 11:50:35 +0100

    feat: add bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
 // docs: http://webpack.github.io/docs/configuration.html
-'use strict';
+"use strict";
 
-const path = require('path');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const devMode = process.env.NODE_ENV !== 'production';
-const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
-const entry = ['whatwg-fetch', './src/index'];
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+    .BundleAnalyzerPlugin;
+const devMode = process.env.NODE_ENV !== "production";
+const mode =
+    process.env.NODE_ENV === "production" ? "production" : "development";
+const entry = ["whatwg-fetch", "./src/index"];
 const plugins = [
     new MiniCssExtractPlugin({
         // Options similar to the same options in webpackOptions.output
         // both options are optional
-        filename: 'bundle.css',
+        filename: "bundle.css"
     }),
     new webpack.DefinePlugin({
-        'process.env': {
-            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-        },
+        "process.env": {
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development")
+        }
     }),
     new webpack.optimize.ModuleConcatenationPlugin(),
     new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin()
 ];
 
 if (devMode) {
-    entry.push('webpack-dev-server/client?http://localhost:3000');
-    entry.push('webpack/hot/only-dev-server');
+    entry.push("webpack-dev-server/client?http://localhost:3000");
+    entry.push("webpack/hot/only-dev-server");
     plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 
@@ -36,13 +40,13 @@ module.exports = {
     entry,
 
     resolve: {
-        extensions: ['.scss', '.css', '.js', '.jsx', '.json'],
+        extensions: [".scss", ".css", ".js", ".jsx", ".json"]
     },
 
     output: {
-        path: path.join(__dirname, 'dist/public'),
-        filename: 'bundle.js',
-        publicPath: '/static/',
+        path: path.join(__dirname, "dist/public"),
+        filename: "bundle.js",
+        publicPath: "/static/"
     },
 
     optimization: {
@@ -50,16 +54,16 @@ module.exports = {
             new UglifyJsPlugin({
                 uglifyOptions: {
                     output: {
-                        comments: false,
-                    },
-                },
+                        comments: false
+                    }
+                }
             }),
             new OptimizeCssAssetsPlugin({
-                cssProcessor: require('cssnano'),
+                cssProcessor: require("cssnano"),
                 cssProcessorOptions: { discardComments: { removeAll: true } },
-                canPrint: true,
-            }),
-        ],
+                canPrint: true
+            })
+        ]
     },
 
     module: {
@@ -67,62 +71,73 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
-                include: path.join(__dirname, 'src'),
+                loader: "babel-loader",
+                include: path.join(__dirname, "src")
             },
             {
                 test: /(\.scss)$/,
                 use: [
-                    { loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader },
                     {
-                        loader: 'css-loader',
+                        loader: devMode
+                            ? "style-loader"
+                            : MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
                         options: {
                             sourceMap: true,
                             modules: true,
                             importLoaders: 1,
-                            localIdentName: '[name]__[local]___[hash:base64:5]',
-                        },
+                            localIdentName: "[name]__[local]___[hash:base64:5]"
+                        }
                     },
                     {
-                        loader: 'sass-loader',
+                        loader: "sass-loader",
                         options: {
                             // data: '@import "theme/_config.scss";',
-                            includePaths: [path.resolve(__dirname, './src')],
-                        },
-                    },
-                ],
+                            includePaths: [path.resolve(__dirname, "./src")]
+                        }
+                    }
+                ]
             },
             {
                 test: /(\.css)$/,
-                use: [{ loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader }, { loader: 'css-loader' }],
-            },
-        ],
+                use: [
+                    {
+                        loader: devMode
+                            ? "style-loader"
+                            : MiniCssExtractPlugin.loader
+                    },
+                    { loader: "css-loader" }
+                ]
+            }
+        ]
     },
 
     plugins,
 
-    devtool: 'source-map',
+    devtool: "source-map",
 
     devServer: {
         proxy: {
-            '/api': {
-                target: process.env.UNLEASH_API || 'http://localhost:4242',
+            "/api": {
+                target: process.env.UNLEASH_API || "http://localhost:4242",
                 changeOrigin: true,
-                secure: false,
+                secure: false
             },
-            '/logout': {
-                target: process.env.UNLEASH_API || 'http://localhost:4242',
+            "/logout": {
+                target: process.env.UNLEASH_API || "http://localhost:4242",
                 changeOrigin: true,
-                secure: false,
+                secure: false
             },
-            '/auth': {
-                target: process.env.UNLEASH_API || 'http://localhost:4242',
+            "/auth": {
+                target: process.env.UNLEASH_API || "http://localhost:4242",
                 changeOrigin: true,
-                secure: false,
-            },
+                secure: false
+            }
         },
         port: process.env.PORT || 3000,
-        host: '0.0.0.0',
-        disableHostCheck: true,
-    },
+        host: "0.0.0.0",
+        disableHostCheck: true
+    }
 };
