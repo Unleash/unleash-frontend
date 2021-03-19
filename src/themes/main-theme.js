36b3f9684e382b450a43497cbd17831dd2699e11
Fredrik Oseberg <fredrik.no@gmail.com>
2021-03-19 15:01:20 +0100

    fix: add new primary color

--- src/themes/main-theme.js
+++ src/themes/main-theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: "#009688",
+            main: "#607d8b",
             light: "#B2DFDB",
             dark: "#00796B"
         },
