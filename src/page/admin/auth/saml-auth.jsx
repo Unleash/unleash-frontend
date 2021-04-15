7e2f0772c4fef3bfb0dc8634a4c8d1d2ac39d4ed
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-04-15 22:07:34 +0200

    fix: project mgm access

--- src/page/admin/auth/saml-auth.jsx
+++ src/page/admin/auth/saml-auth.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Grid, Switch, TextField, Typography } from '@material-ui/core';
+import { Button, Grid, Switch, TextField } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 import PageContent from '../../../component/common/PageContent/PageContent';
 
