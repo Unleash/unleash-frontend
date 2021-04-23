cb130208cb1a6f7adbb284f4bbdb2278fcfa33a7
Fredrik Oseberg <fredrik.no@gmail.com>
2021-04-23 13:35:49 +0200

    feat: add link to docs

--- src/component/menu/Header/Header.jsx
+++ src/component/menu/Header/Header.jsx
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
 import { Route } from 'react-router-dom';
-import { AppBar, Container, Typography, IconButton } from '@material-ui/core';
+import {
+    AppBar,
+    Container,
+    Typography,
+    IconButton,
+    Tooltip,
+} from '@material-ui/core';
 import { DrawerMenu } from '../drawer';
 import MenuIcon from '@material-ui/icons/Menu';
 import Breadcrumb from '../breadcrumb';
-import ShowUserContainer from '../../user/UserProfile';
+import UserProfile from '../../user/UserProfile';
 import ConditionallyRender from '../../common/ConditionallyRender/ConditionallyRender';
+import HelpIcon from '@material-ui/icons/Help';
 
 import { useStyles } from './styles';
 
@@ -50,7 +57,17 @@ const Header = ({ uiConfig, init }) => {
                     />
 
                     <div className={styles.userContainer}>
-                        <ShowUserContainer />
+                        <Tooltip title="Go to the documentation">
+                            <a
+                                href="https://docs.getunleash.io/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className={styles.docsLink}
+                            >
+                                <HelpIcon className={styles.docsIcon} />
+                            </a>
+                        </Tooltip>
+                        <UserProfile />
                     </div>
                     <DrawerMenu
                         links={links}
