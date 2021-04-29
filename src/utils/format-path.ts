76b8a851cdb3a6f6f159b92b34ad0666dcc8e7a0
Fredrik Oseberg <fredrik.no@gmail.com>
2021-04-29 18:58:18 +0200

    fix: delete path formatter

--- src/utils/format-path.ts
+++ src/utils/format-path.ts
@@ -13,39 +13,3 @@ export const formatApiPath = (path: string) => {
     }
     return path;
 };
-
-export const formatPathUtil = (path: string, basePath?: string): string => {
-    // If no basepath, return the formatted path
-    if (!basePath) {
-        // If path exists than one and is already absolute
-        // return a path in the format of '/path'
-        if (path && path[0] === '/') {
-            return path;
-        }
-
-        // If path is greater than one and is relative. Return
-        // an absolutely formatted path in the format of '/path'
-        if (path) {
-            return `/${path}`;
-        }
-    }
-
-    // If basePath is absolute, remove the beginning slash.
-    let formattedBasePath =
-        basePath && basePath[0] === '/' ? basePath?.substr(1) : basePath;
-
-    // If path exists and path is already absolute, remove the beginning
-    // slash and format the path with the basePath first.
-    if (path && path[0] === '/') {
-        let tempPath = path.substr(1);
-
-        return `/${formattedBasePath}/#/${tempPath}`;
-    }
-
-    // If basePath exists and path is relative return formatted path.
-    if (path) {
-        return `/${formattedBasePath}/#/${path}`;
-    }
-
-    return path;
-};
