68856a2cb6d59cbcdd4529fe3a16057ab61bc5a9
Fredrik Oseberg <fredrik.no@gmail.com>
2021-05-05 11:08:50 +0200

    fix: remove project display check

--- src/component/common/ProjectSelect/ProjectSelect.jsx
+++ src/component/common/ProjectSelect/ProjectSelect.jsx
@@ -9,15 +9,12 @@ const ProjectSelect = ({
     projects,
     currentProjectId,
     updateCurrentProject,
+    ...rest
 }) => {
     const setProject = v => {
         const id = typeof v === 'string' ? v.trim() : '';
         updateCurrentProject(id);
     };
-
-    if (!projects || projects.length === 1) {
-        return null;
-    }
 
     // TODO fixme
     let curentProject = projects.find(i => i.id === currentProjectId);
@@ -66,6 +63,7 @@ const ProjectSelect = ({
                 callback={handleChangeProject}
                 renderOptions={renderProjectOptions}
                 className=""
+                {...rest}
             />
         </React.Fragment>
     );
