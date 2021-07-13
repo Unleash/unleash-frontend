4b2e165adaa865b3fc5e955017fb65009a6f655e
Fredrik Oseberg <fredrik.no@gmail.com>
2021-07-13 11:44:06 +0200

    feat: add navigation menu

--- src/component/menu/Header/Header.tsx
+++ src/component/menu/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
@@ -9,20 +9,73 @@ import {
     Typography,
     IconButton,
     Tooltip,
+    Button,
+    Menu,
+    MenuItem,
 } from '@material-ui/core';
 import { DrawerMenu } from '../drawer';
 import MenuIcon from '@material-ui/icons/Menu';
 import Breadcrumb from '../breadcrumb';
+import SettingsIcon from '@material-ui/icons/Settings';
 import UserProfile from '../../user/UserProfile';
 import ConditionallyRender from '../../common/ConditionallyRender/ConditionallyRender';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
-import { ReactComponent as UnleashLogo } from '../../../assets/img/logo-with-name.svg';
+import { ReactComponent as UnleashLogo } from '../../../assets/img/logo-dark-with-text.svg';
 import { useStyles } from './Header.styles';
 import useUiConfig from '../../../hooks/api/getters/useUiConfig/useUiConfig';
 import { useCommonStyles } from '../../../common.styles';
+import { ADMIN, EDITOR } from '../../AccessProvider/permissions';
+import useUser from '../../../hooks/api/getters/useUser/useUser';
+import { IPermission } from '../../../interfaces/user';
+import NavigationMenu from './NavigationMenu/NavigationMenu';
+
+const options = [
+    {
+        path: '/admin/users',
+        permission: ADMIN,
+        text: 'Users and roles',
+    },
+    {
+        path: '/admin/api',
+        permission: ADMIN,
+        text: 'API access',
+    },
+    {
+        path: '/admin/auth',
+        permission: ADMIN,
+        text: 'Authentication',
+    },
+    {
+        path: '/history',
+        permission: ADMIN,
+        text: 'Event log',
+    },
+    {
+        path: '/addons',
+        permission: EDITOR,
+        text: 'Addons',
+    },
+];
+
+const advancedOptions = [
+    {
+        path: '/context',
+        permission: EDITOR,
+        text: 'Context fields',
+    },
+    {
+        path: '/tag-types',
+        permission: EDITOR,
+        text: 'Tag types',
+    },
+];
 
 const Header = () => {
     const theme = useTheme();
+    const [anchorEl, setAnchorEl] = useState();
+    const [anchorElAdvanced, setAnchorElAdvanced] = useState();
+    const [admin, setAdmin] = useState(false);
+    const { permissions } = useUser();
     const commonStyles = useCommonStyles();
     const { uiConfig } = useUiConfig();
     const smallScreen = useMediaQuery(theme.breakpoints.down('sm'));
@@ -30,6 +83,25 @@ const Header = () => {
     const [openDrawer, setOpenDrawer] = useState(false);
 
     const toggleDrawer = () => setOpenDrawer(prev => !prev);
+    const handleClose = () => setAnchorEl(null);
+    const handleCloseAdvanced = () => setAnchorElAdvanced(null);
+
+    useEffect(() => {
+        const admin = permissions.find(
+            (element: IPermission) => element.permission === ADMIN
+        );
+
+        if (admin) {
+            setAdmin(true);
+        }
+    }, [permissions]);
+
+    const selectOptions = () => {
+        if (admin) {
+            return options;
+        }
+        return options.filter(option => option.permission !== ADMIN);
+    };
 
     const { links, name, flags } = uiConfig;
 
@@ -44,7 +116,7 @@ const Header = () => {
                         <MenuIcon />
                     </IconButton> */}
                     <Link to="/" className={commonStyles.flexRow}>
-                        <UnleashLogo />
+                        <UnleashLogo className={styles.logo} />
                     </Link>
                     {/* <ConditionallyRender
                         condition={!smallScreen}
@@ -60,9 +132,23 @@ const Header = () => {
 
                     <div className={styles.links}>
                         <Link to="/reporting">Dashboard</Link>
-                        <Link to="/projects-new">Projects</Link>
+                        <ConditionallyRender
+                            condition={flags.P}
+                            show={<Link to="/projects-new">Projects</Link>}
+                        />
+
                         <Link to="/applications">Applications</Link>
-                        <Link to="/api">Advanced</Link>
+                        <Link
+                            onClick={e => setAnchorElAdvanced(e.currentTarget)}
+                        >
+                            Advanced
+                        </Link>
+                        <NavigationMenu
+                            id="settings-navigation"
+                            options={advancedOptions}
+                            anchorEl={anchorElAdvanced}
+                            handleClose={handleCloseAdvanced}
+                        />
                     </div>
 
                     <div className={styles.userContainer}>
@@ -76,6 +162,15 @@ const Header = () => {
                                 <MenuBookIcon className={styles.docsIcon} />
                             </a>
                         </Tooltip>
+                        <IconButton onClick={e => setAnchorEl(e.currentTarget)}>
+                            <SettingsIcon className={styles.docsIcon} />
+                        </IconButton>
+                        <NavigationMenu
+                            id="settings-navigation"
+                            options={selectOptions()}
+                            anchorEl={anchorEl}
+                            handleClose={handleClose}
+                        />
 
                         <UserProfile />
                     </div>
---
+++ src/component/menu/Header/NavigationMenu/NavigationMenu.tsx
@@ -0,0 +1,55 @@
+import { Menu, MenuItem } from '@material-ui/core';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useStyles } from './NavigationMenu.styles';
+
+interface INavigationMenuOptions {
+    permission: string;
+    path: string;
+    text: string;
+}
+
+interface INavigationMenuProps {
+    options: INavigationMenuOptions[];
+    id: string;
+    anchorEl: any;
+    handleClose: () => void;
+}
+
+const NavigationMenu = ({
+    options,
+    id,
+    handleClose,
+    anchorEl,
+}: INavigationMenuProps) => {
+    const styles = useStyles();
+
+    console.log(anchorEl);
+
+    return (
+        <Menu
+            id={id}
+            onClose={handleClose}
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            style={{ top: '45px', left: '-90px' }}
+        >
+            {options.map(option => {
+                return (
+                    <MenuItem
+                        className={styles.menuItem}
+                        key={option.path}
+                        onClick={handleClose}
+                    >
+                        <Link className={styles.navMenuLink} to={option.path}>
+                            <span className={styles.menuItemBox} />
+                            {option.text}
+                        </Link>
+                    </MenuItem>
+                );
+            })}
+        </Menu>
+    );
+};
+
+export default NavigationMenu;
