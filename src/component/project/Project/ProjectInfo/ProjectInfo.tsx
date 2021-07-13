dd3b7735f3b2fabc2bfec740dbf7dd1fcf7d072c
Fredrik Oseberg <fredrik.no@gmail.com>
2021-07-13 14:00:10 +0200

    fix: round corners

--- src/component/project/Project/ProjectInfo/ProjectInfo.tsx
+++ src/component/project/Project/ProjectInfo/ProjectInfo.tsx
@@ -33,12 +33,9 @@ const ProjectInfo = ({
 
     return (
         <aside>
-            <Paper className={styles.projectInfo}>
+            <div className={styles.projectInfo}>
                 <div className={styles.infoSection} data-loading>
-                    <ProjectIcon />
-                </div>
-
-                <div className={styles.infoSection} data-loading>
+                    <ProjectIcon className={styles.projectIcon} />
                     <p className={styles.subtitle}>Overall health rating</p>
                     <p className={styles.emphazisedText}>{health}%</p>
                     <Link
@@ -55,6 +52,15 @@ const ProjectInfo = ({
                 </div>
 
                 <div className={styles.infoSection} data-loading>
+                    <p className={styles.subtitle}>Feature toggles</p>
+                    <p className={styles.emphazisedText}>{featureCount}</p>
+                </div>
+
+                <div
+                    className={styles.infoSection}
+                    style={{ marginBottom: '0' }}
+                    data-loading
+                >
                     <p className={styles.subtitle}>Project members</p>
                     <p className={styles.emphazisedText}>{memberCount}</p>
                     <Link
@@ -69,12 +75,7 @@ const ProjectInfo = ({
                         <ArrowForwardIcon className={styles.arrowIcon} />
                     </Link>
                 </div>
-
-                <div className={styles.infoSection} data-loading>
-                    <p className={styles.subtitle}>Feature toggles</p>
-                    <p className={styles.emphazisedText}>{featureCount}</p>
-                </div>
-            </Paper>
+            </div>
         </aside>
     );
 };
