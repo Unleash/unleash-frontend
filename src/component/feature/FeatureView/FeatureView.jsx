123606594b56ce777f4b4d870f1fd7a8ed6e7c62
Fredrik Oseberg <fredrik.no@gmail.com>
2021-08-24 15:20:17 +0200

    fix: update test data

--- src/component/feature/FeatureView/FeatureView.jsx
+++ src/component/feature/FeatureView/FeatureView.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useLayoutEffect, useState } from 'react';
+import { useContext, useEffect, useLayoutEffect, useState } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
