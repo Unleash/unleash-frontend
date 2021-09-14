02b3c9ce6a674dde308ab2b3d63ffd4ac05a550e
Fredrik Oseberg <fredrik.no@gmail.com>
2021-09-14 13:53:24 +0200

    fix: update tests

--- src/component/menu/__tests__/routes-test.jsx
+++ src/component/menu/__tests__/routes-test.jsx
@@ -1,7 +1,7 @@
 import { baseRoutes, getRoute } from '../routes';
 
 test('returns all baseRoutes', () => {
-    expect(baseRoutes).toHaveLength(37);
+    expect(baseRoutes).toHaveLength(38);
     expect(baseRoutes).toMatchSnapshot();
 });
 
