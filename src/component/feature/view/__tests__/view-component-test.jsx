32e073fa6e763651cdc4afc47cf40a353a381071
Fredrik Oseberg <fredrik.no@gmail.com>
2021-10-07 16:26:49 +0200

    fix: update snapshots

--- src/component/feature/view/__tests__/view-component-test.jsx
+++ src/component/feature/view/__tests__/view-component-test.jsx
@@ -21,9 +21,12 @@ jest.mock('../update-strategies-container', () => ({
     __esModule: true,
     default: 'UpdateStrategiesComponent',
 }));
-jest.mock('../../feature-type-select-container', () => 'FeatureTypeSelect');
+jest.mock(
+    '../../../feature/FeatureView2/FeatureSettings/FeatureSettingsMetadata/FeatureTypeSelect/FeatureTypeSelect',
+    () => 'FeatureTypeSelect'
+);
 jest.mock('../../../common/ProjectSelect', () => 'ProjectSelect');
-jest.mock('../../tag-type-select-container', () => 'TagSelect');
+jest.mock('../../../common/TagSelect/TagSelect', () => 'TagSelect');
 jest.mock('../../feature-tag-component', () => 'FeatureTagComponent');
 jest.mock('../../add-tag-dialog-container', () => 'AddTagDialog');
 
@@ -60,6 +63,7 @@ test('renders correctly with one feature', () => {
                 },
             }),
         },
+        featureTypes: { toJS: () => [{ id: 'release', name: 'Release' }] },
     };
 
     const mockReducer = state => state;
