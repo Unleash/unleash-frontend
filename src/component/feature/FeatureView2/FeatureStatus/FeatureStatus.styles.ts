f8f59c5e3534959dccd455ba01f8c9e36d75d28a
Fredrik Oseberg <fredrik.no@gmail.com>
2021-10-08 13:23:40 +0200

    fix: text size

--- src/component/feature/FeatureView2/FeatureStatus/FeatureStatus.styles.ts
+++ src/component/feature/FeatureView2/FeatureStatus/FeatureStatus.styles.ts
@@ -2,14 +2,15 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(theme => ({
     container: {
-        display: 'inline-block',
         width: '42px',
         height: '42px',
         fontSize: '0.7em',
         background: 'gray',
         borderRadius: '3px',
         textAlign: 'center',
-        verticalAlign: 'middle',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
         padding: '13px 10px',
     },
 }));
