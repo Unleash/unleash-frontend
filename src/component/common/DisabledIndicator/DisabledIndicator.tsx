5ce427d1cdfb5431718c3bcf0e9a4ff80fc46e7c
Fredrik Oseberg <fredrik.no@gmail.com>
2021-11-23 14:14:21 +0100

    fix: add style file to disabled indicator

--- src/component/common/DisabledIndicator/DisabledIndicator.tsx
+++ src/component/common/DisabledIndicator/DisabledIndicator.tsx
@@ -1,21 +1,13 @@
+import { useStyles } from './DisabledIndicator.styles';
+import classnames from 'classnames';
 interface IDisabledIndicator {
     className?: string;
 }
 
 const DisabledIndicator = ({ className }: IDisabledIndicator) => {
+    const styles = useStyles();
     return (
-        <span
-            style={{
-                padding: '0.2rem',
-                borderRadius: '5px',
-                marginLeft: '0.5rem',
-                backgroundColor: '#000',
-                color: '#fff',
-                fontSize: '0.9rem',
-                fontWeight: 'bold',
-            }}
-            className={className}
-        >
+        <span className={classnames(styles.indicator, className)}>
             disabled
         </span>
     );
