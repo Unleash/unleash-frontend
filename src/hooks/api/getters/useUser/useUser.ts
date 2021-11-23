cd61ed226f385b0bdcd94d21bcb2b09d47b8008d
Youssef Khedher <khedher.youssef@hotmail.fr>
2021-11-23 21:52:24 +0100

    fix: add SWR configuration

--- src/hooks/api/getters/useUser/useUser.ts
+++ src/hooks/api/getters/useUser/useUser.ts
@@ -6,7 +6,13 @@ import handleErrorResponses from '../httpErrorResponseHandler';
 
 export const USER_CACHE_KEY = `api/admin/user`;
 
-const useUser = (options: SWRConfiguration = {revalidateOnFocus: false}) => {
+const useUser = (
+    options: SWRConfiguration = {
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+    }
+) => {
     const fetcher = () => {
         const path = formatApiPath(`api/admin/user`);
         return fetch(path, {
