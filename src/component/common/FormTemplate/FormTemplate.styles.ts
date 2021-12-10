ef9998026728dfb37d16cc834bcaad2dffababcb
Fredrik Oseberg <fredrik.no@gmail.com>
2021-12-10 13:18:39 +0100

    fix: add codebox

--- src/component/common/FormTemplate/FormTemplate.styles.ts
+++ src/component/common/FormTemplate/FormTemplate.styles.ts
@@ -2,44 +2,52 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(theme => ({
     container: {
-        minHeight: "80vh", 
-        width: "100%",
-        display: "flex",
-        maxWidth: "90%",
-        margin: "0 auto"
+        minHeight: '80vh',
+        width: '100%',
+        display: 'flex',
+        maxWidth: '90%',
+        margin: '0 auto',
     },
     sidebar: {
         backgroundColor: theme.palette.primary.light,
-        padding: "2rem",
-        width: "35%",
-        borderTopLeftRadius: "12.5px",
-        borderBottomLeftRadius: "12.5px"
+        padding: '2rem',
+        width: '35%',
+        borderTopLeftRadius: '12.5px',
+        borderBottomLeftRadius: '12.5px',
     },
     title: {
-        color: "#fff",
-        marginBottom: "1rem"
+        color: '#fff',
+        marginBottom: '1rem',
+    },
+    subtitle: {
+        color: '#fff',
+        marginBottom: '1rem',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
     description: {
-        color: "#fff"
+        color: '#fff',
     },
     linkContainer: {
-        margin: "1.5rem 0",
-        display: "flex",
-        alignItems: "center"
+        margin: '1.5rem 0',
+        display: 'flex',
+        alignItems: 'center',
     },
     linkIcon: {
-        marginRight: "0.5rem",
-        color: "#fff"
+        marginRight: '0.5rem',
+        color: '#fff',
     },
     documentationLink: {
-        color: "#fff",
-        display: "block",
+        color: '#fff',
+        display: 'block',
     },
     formContent: {
-        backgroundColor: "#fff",
-        padding: "2rem",
-        width: "65%",
-        borderTopRightRadius: "12.5px",
-        borderBottomRightRadius: "12.5px"
-    }
+        backgroundColor: '#fff',
+        padding: '2rem',
+        width: '65%',
+        borderTopRightRadius: '12.5px',
+        borderBottomRightRadius: '12.5px',
+    },
+    icon: { fill: '#fff' },
 }));
--- src/component/common/FormTemplate/FormTemplate.tsx
+++ src/component/common/FormTemplate/FormTemplate.tsx
@@ -1,5 +1,8 @@
 import { useStyles } from './FormTemplate.styles';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
+import Codebox from '../Codebox/Codebox';
+import { IconButton } from '@material-ui/core';
+import { FileCopy } from '@material-ui/icons';
 
 interface ICreateProps {
     title: string;
@@ -31,6 +34,16 @@ const FormTemplate: React.FC<ICreateProps> = ({
                         Learn more
                     </a>
                 </div>
+                <h3 className={styles.subtitle}>
+                    API Command{' '}
+                    <IconButton className={styles.iconButton}>
+                        <FileCopy className={styles.icon} />
+                    </IconButton>
+                </h3>
+                <Codebox
+                    text='curl https://app.unleash-hosted.com/demo/api/client/features \
+-H "Authorization: 56907a2fa53c1d16101d509a10b78e36190b0f918d9f122d";'
+                />
             </aside>
             <div className={styles.formContent}>{children}</div>
         </section>
