8c120648972430c4698f7bbd857524f74b706173
Fredrik Oseberg <fredrik.no@gmail.com>
2022-01-28 17:12:16 +0100

    fix: update cache key

--- src/hooks/api/getters/useUiBootstrap/useUiBootstrap.ts
+++ src/hooks/api/getters/useUiBootstrap/useUiBootstrap.ts
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { formatApiPath } from '../../../../utils/format-path';
 
 const useUiBootstrap = (options: SWRConfiguration = {}) => {
-    const USER_CACHE_KEY = `api/admin/ui-bootstrap`;
+    const BOOTSTRAP_CACHE_KEY = `api/admin/ui-bootstrap`;
 
     const fetcher = () => {
         const path = formatApiPath(`api/admin/ui-bootstrap`);
@@ -17,11 +17,11 @@ const useUiBootstrap = (options: SWRConfiguration = {}) => {
             .then(res => res.json());
     };
 
-    const { data, error } = useSWR(USER_CACHE_KEY, fetcher, options);
+    const { data, error } = useSWR(BOOTSTRAP_CACHE_KEY, fetcher, options);
     const [loading, setLoading] = useState(!error && !data);
 
     const refetchUiBootstrap = () => {
-        mutate(USER_CACHE_KEY);
+        mutate(BOOTSTRAP_CACHE_KEY);
     };
 
     useEffect(() => {
