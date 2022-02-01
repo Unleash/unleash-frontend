0438d194a9861e05ddbf21602bed34e2d2388022
olav <mail@olav.io>
2022-02-01 15:44:03 +0100

    refactor: move ApiDetails to the Footer dir

--- src/component/menu/__tests__/api-details.test.tsx
+++ src/component/menu/__tests__/api-details.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { ApiDetails } from '../ApiDetails/ApiDetails';
+import { ApiDetails } from '../Footer/ApiDetails';
 
 test('renders correctly with empty version', () => {
     const uiConfig = {
