fbf8a9fd47fc46cbe72312f374b0f96f19594b1a
olav <mail@olav.io>
2022-02-08 11:32:46 +0100

    refactor: improve storage helper comments

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,20 +1,20 @@
 // Get an item from localStorage.
-// Returns undefined if the browser denies access to localStorage.
+// Returns undefined if the browser denies access.
 export function getLocalStorageItem<T>(key: string): T | undefined {
     try {
         return parseStoredItem<T>(window.localStorage.getItem(key));
     } catch (err: unknown) {
-        console.error(err);
+        console.warn(err);
     }
 }
 
-// Store an item from localStorage.
-// Does nothing if the browser denies access to localStorage.
+// Store an item in localStorage.
+// Does nothing if the browser denies access.
 export function setLocalStorageItem(key: string, value: unknown) {
     try {
         window.localStorage.setItem(key, JSON.stringify(value));
     } catch (err: unknown) {
-        console.error(err);
+        console.warn(err);
     }
 }
 
@@ -24,6 +24,6 @@ function parseStoredItem<T>(data: string | null): T | undefined {
     try {
         return data ? JSON.parse(data) : undefined;
     } catch (err: unknown) {
-        console.error(err);
+        console.warn(err);
     }
 }
