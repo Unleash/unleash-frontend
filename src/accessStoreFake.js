cdfe1356e64cfd86f6fe2f6f6c7874eed8b4d0e7
Fredrik Oseberg <fredrik.no@gmail.com>
2022-02-10 14:49:35 +0100

    fix: missing store

---
+++ src/accessStoreFake.js
@@ -0,0 +1,11 @@
+import { Map as $MAp } from 'immutable';
+
+export const createFakeStore = permissions => {
+    return {
+        getState: () => ({
+            user: new $MAp({
+                permissions,
+            }),
+        }),
+    };
+};
