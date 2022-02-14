8623419dea00c17d12211836b704c921f2bbf3e4
olav <mail@olav.io>
2022-02-14 14:38:26 +0100

    refactor: add success toast on stale toggle

--- src/component/feature/FeatureView/FeatureOverview/StaleDialog/StaleDialog.tsx
+++ src/component/feature/FeatureView/FeatureOverview/StaleDialog/StaleDialog.tsx
@@ -5,6 +5,8 @@ import { DialogContentText } from '@material-ui/core';
 import ConditionallyRender from '../../../../common/ConditionallyRender/ConditionallyRender';
 import Dialogue from '../../../../common/Dialogue';
 import useFeature from '../../../../../hooks/api/getters/useFeature/useFeature';
+import React from 'react';
+import useToast from '../../../../../hooks/useToast';
 
 interface IStaleDialogProps {
     open: boolean;
@@ -13,6 +15,7 @@ interface IStaleDialogProps {
 }
 
 const StaleDialog = ({ open, setOpen, stale }: IStaleDialogProps) => {
+    const { setToastData } = useToast();
     const { projectId, featureId } = useParams<IFeatureViewParams>();
     const { patchFeatureToggle } = useFeatureApi();
     const { refetch } = useFeature(projectId, featureId);
@@ -30,12 +33,26 @@ const StaleDialog = ({ open, setOpen, stale }: IStaleDialogProps) => {
 
     const toggleActionText = stale ? 'active' : 'stale';
 
-    const onSubmit = async e => {
-        e.stopPropagation();
+    const onSubmit = async (event: React.SyntheticEvent) => {
+        event.stopPropagation();
         const patch = [{ op: 'replace', path: '/stale', value: !stale }];
         await patchFeatureToggle(projectId, featureId, patch);
         refetch();
         setOpen(false);
+
+        if (stale) {
+            setToastData({
+                type: 'success',
+                title: "And we're back!",
+                text: 'The toggle is no longer marked as stale.',
+            });
+        } else {
+            setToastData({
+                type: 'success',
+                title: 'A job well done.',
+                text: 'The toggle has been marked as stale.',
+            });
+        }
     };
 
     const onCancel = () => {
