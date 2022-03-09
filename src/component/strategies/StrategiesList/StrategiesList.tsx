654eb52fe065701dbcb7d1b04d0bf52a2e2677b0
olav <mail@olav.io>
2022-03-09 09:58:06 +0100

    refactor: fix unused ts-expect-error

--- src/component/strategies/StrategiesList/StrategiesList.tsx
+++ src/component/strategies/StrategiesList/StrategiesList.tsx
@@ -206,7 +206,6 @@ export const StrategiesList = () => {
                         history.push(`/strategies/${strategy?.name}/edit`)
                     }
                     permission={UPDATE_STRATEGY}
-                    tooltip={'Edit strategy'}
                 >
                     <Edit titleAccess="Edit strategy" />
                 </PermissionIconButton>
