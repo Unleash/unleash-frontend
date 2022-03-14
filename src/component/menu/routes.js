4f2cc83ce758121094592ffd19bcbd023634d310
Youssef <khedher.youssef@hotmail.fr>
2022-03-14 14:44:04 +0100

    feat: add flag

--- src/component/menu/routes.js
+++ src/component/menu/routes.js
@@ -10,7 +10,7 @@ import AdminInvoice from '../admin/invoice/InvoiceAdminPage';
 import AdminUsers from '../admin/users/UsersAdmin';
 import { AuthSettings } from '../admin/auth/AuthSettings';
 import Login from '../user/Login/Login';
-import { C, E, EEA, P, RE } from '../common/flags';
+import { C, E, EEA, P, RE, SE } from '../common/flags';
 import { NewUser } from '../user/NewUser/NewUser';
 import ResetPassword from '../user/ResetPassword/ResetPassword';
 import ForgottenPassword from '../user/ForgottenPassword/ForgottenPassword';
@@ -361,6 +361,7 @@ export const routes = [
         type: 'protected',
         layout: 'main',
         menu: { mobile: true, advanced: true },
+        flag: SE,
     },
 
     // History
