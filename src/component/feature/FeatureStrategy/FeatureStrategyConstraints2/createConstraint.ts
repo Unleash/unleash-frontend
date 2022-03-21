17c743c6a4844851477ba3d0821d371e836c56db
Youssef <khedher.youssef@hotmail.fr>
2022-03-21 12:12:38 +0100

    fix: input date not showing up in constraint view
    Co-authored-by: olav <mail@olav.io>

--- src/component/feature/FeatureStrategy/FeatureStrategyConstraints2/createConstraint.ts
+++ src/component/feature/FeatureStrategy/FeatureStrategyConstraints2/createConstraint.ts
@@ -1,12 +1,18 @@
+import { dateOperators } from 'constants/operators';
 import { IConstraint } from 'interfaces/strategy';
+import { oneOf } from 'utils/one-of';
 import { operatorsForContext } from 'utils/operator-utils';
 
 export const createConstraint = (contextName: string): IConstraint => {
+    const operator = operatorsForContext(contextName)[0];
+    const value = oneOf(dateOperators, operator)
+        ? new Date().toISOString()
+        : '';
     return {
         contextName,
-        operator: operatorsForContext(contextName)[0],
+        operator,
         values: [],
-        value: '',
+        value,
         caseInsensitive: false,
         inverted: false,
     };
