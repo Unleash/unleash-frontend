8d6a0b98be283fcbb9498bb82423537a51a45cea
olav <mail@olav.io>
2022-03-25 14:12:08 +0100

    refactor: fix segment chip text alighment

--- src/component/feature/FeatureStrategy/FeatureStrategySegment/FeatureStrategySegmentChip.styles.ts
+++ src/component/feature/FeatureStrategy/FeatureStrategySegment/FeatureStrategySegmentChip.styles.ts
@@ -7,7 +7,7 @@ export const useStyles = makeStyles(theme => ({
         gap: '0.25rem',
         paddingInlineStart: '1rem',
         paddingInlineEnd: '0.5rem',
-        paddingBlockStart: 2,
+        paddingBlockStart: 4,
         paddingBlockEnd: 4,
         borderRadius: '100rem',
         background: theme.palette.primary.main,
