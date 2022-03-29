a616525abd675931bba0e0e285c11fe7e7c7fbae
olav <mail@olav.io>
2022-03-29 12:48:47 +0200

    refactor: use absolute import paths

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,12 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { StylesProvider } from '@material-ui/core/styles';
 import mainTheme from 'themes/mainTheme';
-import { App } from './component/App';
+import { App } from 'component/App';
 import ScrollToTop from 'component/scrollToTop';
-import AccessProvider from './component/providers/AccessProvider/AccessProvider';
+import AccessProvider from 'component/providers/AccessProvider/AccessProvider';
 import { getBasePath } from 'utils/formatPath';
-import UIProvider from './component/providers/UIProvider/UIProvider';
 import { FeedbackCESProvider } from 'component/feedback/FeedbackCESContext/FeedbackCESProvider';
+import UIProvider from 'component/providers/UIProvider/UIProvider';
 
 ReactDOM.render(
     <DndProvider backend={HTML5Backend}>
