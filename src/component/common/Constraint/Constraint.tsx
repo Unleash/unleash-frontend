235a7f2122d92f672b3c6f5e2a5b6a59f40fac42
olav <mail@olav.io>
2022-04-01 15:32:11 +0200

    refactor: remove pre-CO constraints list

--- src/component/common/Constraint/Constraint.tsx
+++ src/component/common/Constraint/Constraint.tsx
@@ -1,7 +1,5 @@
 import { Delete, Edit } from '@material-ui/icons';
 import classnames from 'classnames';
-import { IN, NOT_IN } from 'constants/operators';
-import useUiConfig from 'hooks/api/getters/useUiConfig/useUiConfig';
 import { useParams } from 'react-router';
 import { IFeatureViewParams } from 'interfaces/params';
 import { IConstraint } from 'interfaces/strategy';
@@ -28,24 +26,15 @@ const Constraint = ({
     className,
     ...rest
 }: IConstraintProps) => {
-    // CHANGEME - Feat: Constraint Operators
-    const { uiConfig } = useUiConfig();
     const styles = useStyles();
     const { locationSettings } = useLocationSettings();
     const { projectId } = useParams<IFeatureViewParams>();
+    const editable = !!(deleteCallback && editCallback);
 
     const classes = classnames(styles.constraint, {
         [styles.column]:
             Array.isArray(constraint.values) && constraint.values.length > 2,
     });
-
-    const editable = !!(deleteCallback && editCallback);
-    // CHANGEME - Feat: Constraint Operators
-    // Disable the edit button for constraints that are using new operators if
-    // the new operators are not enabled
-    const operatorIsNew =
-        constraint.operator !== IN && constraint.operator !== NOT_IN;
-    const disabledEdit = !uiConfig.flags.CO && operatorIsNew;
 
     return (
         <div className={classes + ' ' + className} {...rest}>
@@ -72,7 +61,6 @@ const Constraint = ({
                             onClick={editCallback!}
                             permission={UPDATE_FEATURE}
                             projectId={projectId}
-                            disabled={disabledEdit}
                         >
                             <Edit titleAccess="Edit constraint" />
                         </PermissionIconButton>
