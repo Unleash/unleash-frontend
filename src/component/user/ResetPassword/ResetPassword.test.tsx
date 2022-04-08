f0826ad860f46eda6c8f49c75d02d0fc943af606
olav <mail@olav.io>
2022-04-08 13:15:17 +0200

    refactor: unify test render context

--- src/component/user/ResetPassword/ResetPassword.test.tsx
+++ src/component/user/ResetPassword/ResetPassword.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { INVALID_TOKEN_BUTTON } from 'utils/testIds';
 import React from 'react';
-import { TestContext } from 'utils/testContext';
 import ResetPassword from 'component/user/ResetPassword/ResetPassword';
 import { MemoryRouter } from 'react-router-dom';
 import { INVALID_TOKEN_ERROR } from 'hooks/api/getters/useResetPassword/useResetPassword';
 import { testServerSetup, testServerRoute } from 'utils/testServer';
+import { render } from 'utils/testRenderer';
 
 const server = testServerSetup();
 
@@ -15,11 +15,9 @@ test('should render password auth', async () => {
     });
 
     render(
-        <TestContext>
-            <MemoryRouter initialEntries={['/new-user?token=invalid']}>
-                <ResetPassword />
-            </MemoryRouter>
-        </TestContext>
+        <MemoryRouter initialEntries={['/new-user?token=invalid']}>
+            <ResetPassword />
+        </MemoryRouter>
     );
 
     await screen.findByTestId(INVALID_TOKEN_BUTTON);
