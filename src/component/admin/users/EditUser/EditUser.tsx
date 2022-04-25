44619ac0e4ad3eceab21772dae89f8c371b01c55
olav <mail@olav.io>
2022-04-25 09:01:35 +0200

    Update src/component/admin/users/EditUser/EditUser.tsx
    Co-authored-by: Thomas Heartman <thomas@getunleash.ai>

--- src/component/admin/users/EditUser/EditUser.tsx
+++ src/component/admin/users/EditUser/EditUser.tsx
@@ -77,7 +77,7 @@ const EditUser = () => {
             title="Edit user"
             description="In order to get access to Unleash needs to have an Unleash root role as Admin, Editor or Viewer.
             You can also add the user to projects as member or owner in the specific projects."
-            documentationLink="https://docs.getunleash.io/user_guide/user-management"
+            documentationLink="https://docs.getunleash.io/user_guide/rbac#standard-roles"
             documentationLinkLabel="More about user management"
             formatApiCode={formatApiCode}
         >
