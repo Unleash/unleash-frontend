083a9ad9c744393019b83d1336c80c07300cce4e
Nuno Góis <github@nunogois.com>
2022-04-27 11:47:30 +0100

    sort handler initial implementation
    Co-authored-by: Tymoteusz Czech <Tymek@users.noreply.github.com>

--- src/component/Experiment/EnhancedTable/useSort.test.ts
+++ src/component/Experiment/EnhancedTable/useSort.test.ts
@@ -2,14 +2,9 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import { useSort } from './useSort';
 
 const data = [
-    {
-        name: 'John',
-        age: 20,
-    },
-    {
-        name: 'Jane',
-        age: 22,
-    },
+    { name: 'John', age: 20 },
+    { name: 'Jane', age: 22 },
+    { name: 'Joe', age: 18 },
 ];
 
 describe('useSort', () => {
@@ -20,16 +15,36 @@ describe('useSort', () => {
         expect(result.current.data).toEqual(data);
     });
 
-    it('should return a sort handler', () => {
+    it('should return a sort handler that sorts the data', () => {
         const { result } = renderHook(() =>
             useSort(data, [{ field: 'age', sort: 'number' }])
         );
         expect(result.current.onSort).toBeInstanceOf(Function);
 
         act(() => {
+            result.current.onSort('age');
+        });
+
+        expect(result.current.data).toEqual([
+            { name: 'Joe', age: 18 },
+            { name: 'John', age: 20 },
+            { name: 'Jane', age: 22 },
+        ]);
+    });
+
+    it('should return a sort handler that sorts the data respecting order', () => {
+        const { result } = renderHook(() =>
+            useSort(data, [{ field: 'age', sort: 'number' }])
+        );
+
+        act(() => {
             result.current.onSort('age', 'desc');
         });
 
-        expect(result.current.data).not.toEqual(data);
+        expect(result.current.data).toEqual([
+            { name: 'Jane', age: 22 },
+            { name: 'John', age: 20 },
+            { name: 'Joe', age: 18 },
+        ]);
     });
 });
--- src/component/Experiment/EnhancedTable/useSort.ts
+++ src/component/Experiment/EnhancedTable/useSort.ts
@@ -2,11 +2,9 @@ import { useState } from 'react';
 
 const sortPresetFunctions = {
     string: (a: string, b: string): number => 0, // FIXME: implement
-    number: (a: number, b: number): number => 0, // FIXME: implement
+    number: (a: number, b: number): number => a - b,
     date: (a: Date, b: Date): number => 0, // FIXME: implement
 };
-
-// ...features/table?sort=name&sortOrder=desc
 
 type NonEmptyArray<T> = [T, ...T[]];
 
@@ -15,7 +13,7 @@ export const useSort = <T extends Record<string, unknown>>(
     columns: NonEmptyArray<
         | {
               field: keyof T;
-              sort: keyof typeof sortPresetFunctions;
+              sort: keyof typeof sortPresetFunctions | ((a: T, b: T) => number);
           }
         | {
               field: string;
@@ -28,8 +26,30 @@ export const useSort = <T extends Record<string, unknown>>(
     }
 ) => {
     const [sort, setSort] = useState(defaultSort);
+    const [sortedData, setSortedData] = useState([...data]);
 
-    const onSort = (field: string, order: 'asc' | 'desc') => {};
+    const onSort = (field: string, order: 'asc' | 'desc' = 'asc') => {
+        const columnOptions = columns.find(col => col.field === field);
+        if (!columnOptions) {
+            return console.warn('Column not found');
+        }
 
-    return { data, onSort };
+        if (typeof columnOptions.sort === 'string') {
+            const sortFunction = sortPresetFunctions[columnOptions.sort];
+
+            setSortedData(prev =>
+                prev.sort((a, b) => {
+                    if (order === 'desc') {
+                        // TODO: deal with sort presets and TS correctly
+                        // @ts-expect-error
+                        return sortFunction(b[field], a[field]); // arguments reversed for descending
+                    }
+                    // @ts-expect-error
+                    return sortFunction(a[field], b[field]);
+                })
+            );
+        }
+    };
+
+    return { data: sortedData, onSort };
 };
