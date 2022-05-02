ad0429dba34dadd3f7f8ee19939bb69112d33340
olav <mail@olav.io>
2022-05-02 15:52:41 +0200

    chore: update MUI to v5 (#923)
    * refactor: update mui packages
    
    * refactor: run mui codemods
    
    * refactor: format files after codemods
    
    * refactor: fix broken types
    
    * refactor: clean up theme
    
    * refactor: fix broken tests
    
    * refactor: replace @mui/styles with tss-react
    
    * refactor: move breakpoints into classes for tss
    
    * refactor: fix crash on missing feature description
    
    * refactor: remove void classNames
    
    * refactor: adjust styles to new defaults
    
    * refactor: remove broken rollout slider e2e test
    
    * refactor: fix duplicate e2e testid
    
    * refactor: update makeStyles after rebase
    
    * refactor: add missing snapshot after rebase
    
    * refactor: fix TableCellSortable focus styles
    
    * refactor: use 1.4 as the default line-height
    
    * refactor: hide webkit search field icons
    
    * refactor: fix select box label
    
    * refactor: make AutocompleteBox smaller
    
    * refactor: make heading smaller
    
    * refactor: fix toast close icon color
    
    * refactor: update snapshots
    
    * refactor: add missing test event awaits
    
    * refactor: fix default button line-height

--- src/component/feature/FeatureToggleList/FeatureToggleListItem/FeatureToggleListItem.tsx
+++ src/component/feature/FeatureToggleList/FeatureToggleListItem/FeatureToggleListItem.tsx
@@ -1,8 +1,8 @@
 import { memo } from 'react';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
-import { Chip, ListItem, Tooltip } from '@material-ui/core';
-import { Undo } from '@material-ui/icons';
+import { Chip, ListItem, Tooltip } from '@mui/material';
+import { Undo } from '@mui/icons-material';
 import TimeAgo from 'react-timeago';
 import { IAccessContext } from 'contexts/AccessContext';
 import StatusChip from 'component/common/StatusChip/StatusChip';
@@ -14,9 +14,9 @@ import FeatureStatus from 'component/feature/FeatureView/FeatureStatus/FeatureSt
 import FeatureType from 'component/feature/FeatureView/FeatureType/FeatureType';
 import useProjects from 'hooks/api/getters/useProjects/useProjects';
 import PermissionIconButton from 'component/common/PermissionIconButton/PermissionIconButton';
-import { styles as commonStyles } from 'component/common'; // FIXME: remove
 import { FeatureSchema } from 'openapi';
-import { useStyles } from './styles'; // FIXME: cleanup
+import { styles as themeStyles } from 'component/common';
+import { useStyles } from './styles';
 
 interface IFeatureToggleListItemProps {
     feature: FeatureSchema;
@@ -37,7 +37,7 @@ export const FeatureToggleListItem = memo<IFeatureToggleListItemProps>(
         className,
         ...rest
     }) => {
-        const styles = useStyles();
+        const { classes: styles } = useStyles();
 
         const { projects } = useProjects();
         const isArchive = Boolean(onRevive);
@@ -85,7 +85,7 @@ export const FeatureToggleListItem = memo<IFeatureToggleListItemProps>(
                 <span
                     className={classnames(
                         styles.listItemType,
-                        commonStyles.hideLt600
+                        themeStyles.hideLt600
                     )}
                 >
                     <FeatureType type={type as string} />
@@ -97,12 +97,12 @@ export const FeatureToggleListItem = memo<IFeatureToggleListItemProps>(
                             <Link
                                 to={getTogglePath(feature.project, name)}
                                 className={classnames(
-                                    commonStyles.listLink,
-                                    commonStyles.truncate
+                                    themeStyles.listLink,
+                                    themeStyles.truncate
                                 )}
                             >
                                 <Tooltip title={description || ''}>
-                                    <span className={commonStyles.toggleName}>
+                                    <span className={themeStyles.toggleName}>
                                         {name}&nbsp;
                                     </span>
                                 </Tooltip>
@@ -120,7 +120,7 @@ export const FeatureToggleListItem = memo<IFeatureToggleListItemProps>(
                                     />
                                 </small>
                                 <div>
-                                    <span className={commonStyles.truncate}>
+                                    <span className={themeStyles.truncate}>
                                         <small>{description}</small>
                                     </span>
                                 </div>
@@ -129,7 +129,7 @@ export const FeatureToggleListItem = memo<IFeatureToggleListItemProps>(
                         elseShow={
                             <>
                                 <Tooltip title={description || ''}>
-                                    <span className={commonStyles.toggleName}>
+                                    <span className={themeStyles.toggleName}>
                                         {name}&nbsp;{' '}
                                     </span>
                                 </Tooltip>
@@ -147,7 +147,7 @@ export const FeatureToggleListItem = memo<IFeatureToggleListItemProps>(
                                     />
                                 </small>
                                 <div>
-                                    <span className={commonStyles.truncate}>
+                                    <span className={themeStyles.truncate}>
                                         <small>{description}</small>
                                     </span>
                                 </div>
@@ -158,7 +158,7 @@ export const FeatureToggleListItem = memo<IFeatureToggleListItemProps>(
                 <span
                     className={classnames(
                         styles.listItemStrategies,
-                        commonStyles.hideLt920
+                        themeStyles.hideLt920
                     )}
                 >
                     <StatusChip stale={Boolean(stale)} showActive={false} />
