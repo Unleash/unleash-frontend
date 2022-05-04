20dc586016ef8c98bef23a1e863fd299a15b76ea
Nuno Góis <github@nunogois.com>
2022-05-04 09:35:41 +0100

    fix: formatting

--- src/component/application/ApplicationEdit/ApplicationEdit.test.jsx
+++ src/component/application/ApplicationEdit/ApplicationEdit.test.jsx
@@ -1,4 +1,4 @@
-import {vi} from 'vitest'
+import { vi } from 'vitest';
 import { ApplicationEdit } from './ApplicationEdit';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
@@ -13,17 +13,17 @@ test('renders correctly if no application', () => {
         .create(
             <AccessProviderMock permissions={[{ permission: ADMIN }]}>
                 <ThemeProvider>
-                <AnnouncerProvider>
-                    <UIProvider>
-                        <MemoryRouter initialEntries={['/test']}>
-                            <ApplicationEdit
-                                fetchApplication={() => Promise.resolve({})}
-                                storeApplicationMetaData={vi.fn()}
-                                deleteApplication={vi.fn()}
-                                locationSettings={{ locale: 'en-GB' }}
-                            />
-                        </MemoryRouter>
-                    </UIProvider>
+                    <AnnouncerProvider>
+                        <UIProvider>
+                            <MemoryRouter initialEntries={['/test']}>
+                                <ApplicationEdit
+                                    fetchApplication={() => Promise.resolve({})}
+                                    storeApplicationMetaData={vi.fn()}
+                                    deleteApplication={vi.fn()}
+                                    locationSettings={{ locale: 'en-GB' }}
+                                />
+                            </MemoryRouter>
+                        </UIProvider>
                     </AnnouncerProvider>
                 </ThemeProvider>
             </AccessProviderMock>
@@ -38,58 +38,58 @@ test('renders correctly without permission', () => {
         .create(
             <MemoryRouter>
                 <ThemeProvider>
-                <AnnouncerProvider>
-                    <UIProvider>
-                        <AccessProviderMock
-                            permissions={[{ permission: ADMIN }]}
-                        >
-                            <ApplicationEdit
-                                fetchApplication={() => Promise.resolve({})}
-                                storeApplicationMetaData={vi.fn()}
-                                deleteApplication={vi.fn()}
-                                application={{
-                                    appName: 'test-app',
-                                    instances: [
-                                        {
-                                            instanceId: 'instance-1',
-                                            clientIp: '123.123.123.123',
-                                            lastSeen: '2017-02-23T15:56:49',
-                                            sdkVersion: '4.0',
-                                        },
-                                    ],
-                                    strategies: [
-                                        {
-                                            name: 'StrategyA',
-                                            description: 'A description',
-                                        },
-                                        {
-                                            name: 'StrategyB',
-                                            description: 'B description',
-                                            notFound: true,
-                                        },
-                                    ],
-                                    seenToggles: [
-                                        {
-                                            name: 'ToggleA',
-                                            description: 'this is A toggle',
-                                            enabled: true,
-                                            project: 'default',
-                                        },
-                                        {
-                                            name: 'ToggleB',
-                                            description: 'this is B toggle',
-                                            enabled: false,
-                                            notFound: true,
-                                            project: 'default',
-                                        },
-                                    ],
-                                    url: 'http://example.org',
-                                    description: 'app description',
-                                }}
-                                locationSettings={{ locale: 'en-GB' }}
-                            />
-                        </AccessProviderMock>
-                    </UIProvider>
+                    <AnnouncerProvider>
+                        <UIProvider>
+                            <AccessProviderMock
+                                permissions={[{ permission: ADMIN }]}
+                            >
+                                <ApplicationEdit
+                                    fetchApplication={() => Promise.resolve({})}
+                                    storeApplicationMetaData={vi.fn()}
+                                    deleteApplication={vi.fn()}
+                                    application={{
+                                        appName: 'test-app',
+                                        instances: [
+                                            {
+                                                instanceId: 'instance-1',
+                                                clientIp: '123.123.123.123',
+                                                lastSeen: '2017-02-23T15:56:49',
+                                                sdkVersion: '4.0',
+                                            },
+                                        ],
+                                        strategies: [
+                                            {
+                                                name: 'StrategyA',
+                                                description: 'A description',
+                                            },
+                                            {
+                                                name: 'StrategyB',
+                                                description: 'B description',
+                                                notFound: true,
+                                            },
+                                        ],
+                                        seenToggles: [
+                                            {
+                                                name: 'ToggleA',
+                                                description: 'this is A toggle',
+                                                enabled: true,
+                                                project: 'default',
+                                            },
+                                            {
+                                                name: 'ToggleB',
+                                                description: 'this is B toggle',
+                                                enabled: false,
+                                                notFound: true,
+                                                project: 'default',
+                                            },
+                                        ],
+                                        url: 'http://example.org',
+                                        description: 'app description',
+                                    }}
+                                    locationSettings={{ locale: 'en-GB' }}
+                                />
+                            </AccessProviderMock>
+                        </UIProvider>
                     </AnnouncerProvider>
                 </ThemeProvider>
             </MemoryRouter>
@@ -104,58 +104,58 @@ test('renders correctly with permissions', () => {
         .create(
             <MemoryRouter>
                 <ThemeProvider>
-                <AnnouncerProvider>
-                    <UIProvider>
-                        <AccessProviderMock
-                            permissions={[{ permission: ADMIN }]}
-                        >
-                            <ApplicationEdit
-                                fetchApplication={() => Promise.resolve({})}
-                                storeApplicationMetaData={vi.fn()}
-                                deleteApplication={vi.fn()}
-                                application={{
-                                    appName: 'test-app',
-                                    instances: [
-                                        {
-                                            instanceId: 'instance-1',
-                                            clientIp: '123.123.123.123',
-                                            lastSeen: '2017-02-23T15:56:49',
-                                            sdkVersion: '4.0',
-                                        },
-                                    ],
-                                    strategies: [
-                                        {
-                                            name: 'StrategyA',
-                                            description: 'A description',
-                                        },
-                                        {
-                                            name: 'StrategyB',
-                                            description: 'B description',
-                                            notFound: true,
-                                        },
-                                    ],
-                                    seenToggles: [
-                                        {
-                                            name: 'ToggleA',
-                                            description: 'this is A toggle',
-                                            enabled: true,
-                                            project: 'default',
-                                        },
-                                        {
-                                            name: 'ToggleB',
-                                            description: 'this is B toggle',
-                                            enabled: false,
-                                            notFound: true,
-                                            project: 'default',
-                                        },
-                                    ],
-                                    url: 'http://example.org',
-                                    description: 'app description',
-                                }}
-                                locationSettings={{ locale: 'en-GB' }}
-                            />
-                        </AccessProviderMock>
-                    </UIProvider>
+                    <AnnouncerProvider>
+                        <UIProvider>
+                            <AccessProviderMock
+                                permissions={[{ permission: ADMIN }]}
+                            >
+                                <ApplicationEdit
+                                    fetchApplication={() => Promise.resolve({})}
+                                    storeApplicationMetaData={vi.fn()}
+                                    deleteApplication={vi.fn()}
+                                    application={{
+                                        appName: 'test-app',
+                                        instances: [
+                                            {
+                                                instanceId: 'instance-1',
+                                                clientIp: '123.123.123.123',
+                                                lastSeen: '2017-02-23T15:56:49',
+                                                sdkVersion: '4.0',
+                                            },
+                                        ],
+                                        strategies: [
+                                            {
+                                                name: 'StrategyA',
+                                                description: 'A description',
+                                            },
+                                            {
+                                                name: 'StrategyB',
+                                                description: 'B description',
+                                                notFound: true,
+                                            },
+                                        ],
+                                        seenToggles: [
+                                            {
+                                                name: 'ToggleA',
+                                                description: 'this is A toggle',
+                                                enabled: true,
+                                                project: 'default',
+                                            },
+                                            {
+                                                name: 'ToggleB',
+                                                description: 'this is B toggle',
+                                                enabled: false,
+                                                notFound: true,
+                                                project: 'default',
+                                            },
+                                        ],
+                                        url: 'http://example.org',
+                                        description: 'app description',
+                                    }}
+                                    locationSettings={{ locale: 'en-GB' }}
+                                />
+                            </AccessProviderMock>
+                        </UIProvider>
                     </AnnouncerProvider>
                 </ThemeProvider>
             </MemoryRouter>
