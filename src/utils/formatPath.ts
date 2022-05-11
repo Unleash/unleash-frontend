aa9247760cc91d09726aa3c9a56e805035fab92a
olav <mail@olav.io>
2022-05-11 08:34:14 +0200

    refactor: add UNLEASH_BASE_PATH frontend env var

--- src/utils/formatPath.ts
+++ src/utils/formatPath.ts
@@ -3,11 +3,21 @@ export const formatApiPath = (path: string, base = basePath): string => {
 };
 
 export const formatAssetPath = (path: string, base = basePath): string => {
+    if (import.meta.env.DEV && import.meta.env.BASE_URL !== '/') {
+        // Vite will automatically add BASE_URL to imported assets.
+        return joinPaths(path);
+    }
+
     return joinPaths(base, path);
 };
 
 // Parse the basePath value from the HTML meta tag.
 export const parseBasePath = (value = basePathMetaTagContent()): string => {
+    if (import.meta.env.DEV && import.meta.env.BASE_URL !== '/') {
+        // Use the `UNLEASH_BASE_PATH` env var instead of the meta tag.
+        return joinPaths(import.meta.env.BASE_URL);
+    }
+
     return value === '::baseUriPath::' ? '' : joinPaths(value);
 };
 
