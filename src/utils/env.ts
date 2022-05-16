c1b5d5ae03b690f1b972cbf5c8ff8f6472ce4730
olav <mail@olav.io>
2022-05-16 08:12:57 +0200

    refactor: simplify env helper functions

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,20 +1,9 @@
-export const isLocalhostDomain = (
-    hostname = window.location.hostname
-): boolean => {
-    return hostname === 'localhost';
-};
+export const isLocalhostDomain = (hostname = window.location.hostname) =>
+    hostname === 'localhost';
 
-export const isUnleashDomain = (
-    hostname = window.location.hostname
-): boolean => {
-    return (
-        hostname.endsWith('.getunleash.io') ||
-        hostname.endsWith('.unleash-hosted.com')
-    );
-};
+export const isUnleashDomain = (hostname = window.location.hostname) =>
+    hostname.endsWith('.getunleash.io') ||
+    hostname.endsWith('.unleash-hosted.com');
 
-export const isVercelBranchDomain = (
-    hostname = window.location.hostname
-): boolean => {
-    return hostname.endsWith('.vercel.app');
-};
+export const isVercelBranchDomain = (hostname = window.location.hostname) =>
+    hostname.endsWith('.vercel.app');
