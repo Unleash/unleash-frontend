a2b02cf392a8e20348601d99ff66ab6659b8de94
Tymoteusz Czech <tymek+gpg@getunleash.ai>
2022-05-23 09:52:56 +0200

    enable new project overview

--- src/component/common/Table/SortableTableHeader/CellSortable/SortArrow/SortArrow.styles.ts
+++ src/component/common/Table/SortableTableHeader/CellSortable/SortArrow/SortArrow.styles.ts
@@ -3,7 +3,8 @@ import { makeStyles } from 'tss-react/mui';
 export const useStyles = makeStyles()(theme => ({
     icon: {
         marginLeft: theme.spacing(0.25),
-        marginRight: -theme.spacing(0.5),
+        // marginRight: -theme.spacing(0.5),
+        marginRight: '-4px',
         color: theme.palette.grey[700],
         fontSize: theme.fontSizes.mainHeader,
         verticalAlign: 'middle',
