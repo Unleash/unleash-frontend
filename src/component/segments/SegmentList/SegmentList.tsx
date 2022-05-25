ab528f8e14088b52a76141707f49d0bf2c672d0e
olav <mail@olav.io>
2022-05-25 08:39:07 +0200

    refactor: extract RemoveSegmentButton component

--- src/component/segments/SegmentList/SegmentList.tsx
+++ src/component/segments/SegmentList/SegmentList.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import {
     Table,
     TableBody,
@@ -12,53 +11,22 @@ import { SegmentListItem } from './SegmentListItem/SegmentListItem';
 import { ISegment } from 'interfaces/segment';
 import { useStyles } from './SegmentList.styles';
 import { useSegments } from 'hooks/api/getters/useSegments/useSegments';
-import { useSegmentsApi } from 'hooks/api/actions/useSegmentsApi/useSegmentsApi';
-import useToast from 'hooks/useToast';
-import { formatUnknownError } from 'utils/formatUnknownError';
 import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
 import { PageHeader } from 'component/common/PageHeader/PageHeader';
 import { PageContent } from 'component/common/PageContent/PageContent';
-import { SegmentDelete } from '../SegmentDelete/SegmentDelete';
 import { SegmentDocsWarning } from 'component/segments/SegmentDocs/SegmentDocs';
 import { SegmentEmpty } from 'component/segments/SegmentEmpty/SegmentEmpty';
 import { CreateSegmentButton } from 'component/segments/CreateSegmentButton/CreateSegmentButton';
 
 export const SegmentsList = () => {
-    const { segments = [], refetchSegments } = useSegments();
-    const { deleteSegment } = useSegmentsApi();
+    const { classes: styles } = useStyles();
+    const { segments = [] } = useSegments();
     const { page, pages, nextPage, prevPage, setPageIndex, pageIndex } =
         usePagination(segments, 10);
-    const [currentSegment, setCurrentSegment] = useState<ISegment>();
-    const [delDialog, setDelDialog] = useState(false);
-    const { setToastData, setToastApiError } = useToast();
-
-    const { classes: styles } = useStyles();
-
-    const onDeleteSegment = async () => {
-        if (!currentSegment?.id) return;
-        try {
-            await deleteSegment(currentSegment?.id);
-            await refetchSegments();
-            setToastData({
-                type: 'success',
-                title: 'Successfully deleted segment',
-            });
-        } catch (error: unknown) {
-            setToastApiError(formatUnknownError(error));
-        }
-        setDelDialog(false);
-    };
 
     const renderSegments = () => {
         return page.map((segment: ISegment) => {
-            return (
-                <SegmentListItem
-                    key={segment.id}
-                    segment={segment}
-                    setCurrentSegment={setCurrentSegment}
-                    setDelDialog={setDelDialog}
-                />
-            );
+            return <SegmentListItem key={segment.id} segment={segment} />;
         });
     };
 
@@ -128,17 +96,6 @@ export const SegmentsList = () => {
             <ConditionallyRender
                 condition={segments.length === 0}
                 show={() => <SegmentEmpty />}
-            />
-            <ConditionallyRender
-                condition={Boolean(currentSegment)}
-                show={() => (
-                    <SegmentDelete
-                        segment={currentSegment!}
-                        open={delDialog}
-                        setDeldialogue={setDelDialog}
-                        handleDeleteSegment={onDeleteSegment}
-                    />
-                )}
             />
         </PageContent>
     );
--- src/component/segments/SegmentList/SegmentListItem/SegmentListItem.tsx
+++ src/component/segments/SegmentList/SegmentListItem/SegmentListItem.tsx
@@ -1,27 +1,16 @@
 import { useStyles } from './SegmentListItem.styles';
-import { Box, TableCell, TableRow, Typography } from '@mui/material';
-import { Delete } from '@mui/icons-material';
-import { DELETE_SEGMENT } from 'component/providers/AccessProvider/permissions';
-import PermissionIconButton from 'component/common/PermissionIconButton/PermissionIconButton';
+import { TableCell, TableRow, Typography, Box } from '@mui/material';
 import TimeAgo from 'react-timeago';
 import { ISegment } from 'interfaces/segment';
-import { SEGMENT_DELETE_BTN_ID } from 'utils/testIds';
 import React from 'react';
 import { EditSegmentButton } from 'component/segments/EditSegmentButton/EditSegmentButton';
+import { RemoveSegmentButton } from 'component/segments/RemoveSegmentButton/RemoveSegmentButton';
 
 interface ISegmentListItemProps {
     segment: ISegment;
-    setCurrentSegment: React.Dispatch<
-        React.SetStateAction<ISegment | undefined>
-    >;
-    setDelDialog: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const SegmentListItem = ({
-    segment,
-    setCurrentSegment,
-    setDelDialog,
-}: ISegmentListItemProps) => {
+export const SegmentListItem = ({ segment }: ISegmentListItemProps) => {
     const { classes: styles } = useStyles();
 
     return (
@@ -46,22 +35,10 @@ export const SegmentListItem = ({
                     {segment.createdBy}
                 </Typography>
             </TableCell>
-
             <TableCell align="right">
                 <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
                     <EditSegmentButton segment={segment} />
-                    <PermissionIconButton
-                        data-loading
-                        onClick={() => {
-                            setCurrentSegment(segment);
-                            setDelDialog(true);
-                        }}
-                        permission={DELETE_SEGMENT}
-                        tooltipProps={{ title: 'Remove segment' }}
-                        data-testid={`${SEGMENT_DELETE_BTN_ID}_${name}`}
-                    >
-                        <Delete />
-                    </PermissionIconButton>
+                    <RemoveSegmentButton segment={segment} />
                 </Box>
             </TableCell>
         </TableRow>
