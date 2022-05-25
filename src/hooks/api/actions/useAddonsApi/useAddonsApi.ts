2c12614c6b5adbfe6a7096ee838368de4ae2c165
Fredrik Oseberg <fredrik.no@gmail.com>
2022-05-25 11:46:51 +0200

    fix: update referential equality

--- src/hooks/api/actions/useAddonsApi/useAddonsApi.ts
+++ src/hooks/api/actions/useAddonsApi/useAddonsApi.ts
@@ -1,4 +1,5 @@
 import { IAddon } from 'interfaces/addons';
+import { useCallback } from 'react';
 import useAPI from '../useApi/useApi';
 
 const useAddonsApi = () => {
@@ -38,20 +39,23 @@ const useAddonsApi = () => {
         }
     };
 
-    const updateAddon = async (addonConfig: IAddon) => {
-        const path = `${URI}/${addonConfig.id}`;
-        const req = createRequest(path, {
-            method: 'PUT',
-            body: JSON.stringify(addonConfig),
-        });
-        try {
-            const res = await makeRequest(req.caller, req.id);
+    const updateAddon = useCallback(
+        async (addonConfig: IAddon) => {
+            const path = `${URI}/${addonConfig.id}`;
+            const req = createRequest(path, {
+                method: 'PUT',
+                body: JSON.stringify(addonConfig),
+            });
+            try {
+                const res = await makeRequest(req.caller, req.id);
 
-            return res;
-        } catch (e) {
-            throw e;
-        }
-    };
+                return res;
+            } catch (e) {
+                throw e;
+            }
+        },
+        [createRequest, makeRequest]
+    );
 
     return {
         createAddon,
