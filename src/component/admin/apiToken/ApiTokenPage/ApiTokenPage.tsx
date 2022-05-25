d019fbc336456d1ff56bd84df4f06fee57fb2d4e
olav <mail@olav.io>
2022-05-25 13:27:07 +0200

    refactor: extract ApiTokenDocs component

--- src/component/admin/apiToken/ApiTokenPage/ApiTokenPage.tsx
+++ src/component/admin/apiToken/ApiTokenPage/ApiTokenPage.tsx
@@ -1,8 +1,7 @@
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Button, Box } from '@mui/material';
 import AccessContext from 'contexts/AccessContext';
-import useUiConfig from 'hooks/api/getters/useUiConfig/useUiConfig';
 import { PageContent } from 'component/common/PageContent/PageContent';
 import { PageHeader } from 'component/common/PageHeader/PageHeader';
 import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
@@ -10,16 +9,13 @@ import {
     CREATE_API_TOKEN,
     READ_API_TOKEN,
 } from 'component/providers/AccessProvider/permissions';
-import { useStyles } from './ApiTokenPage.styles';
 import { CREATE_API_TOKEN_BUTTON } from 'utils/testIds';
-import { Alert } from '@mui/material';
 import { ApiTokenList } from 'component/admin/apiToken/ApiTokenList/ApiTokenList';
 import { AdminAlert } from 'component/common/AdminAlert/AdminAlert';
+import { ApiTokenDocs } from 'component/admin/apiToken/ApiTokenDocs/ApiTokenDocs';
 
 export const ApiTokenPage = () => {
-    const { classes: styles } = useStyles();
     const { hasAccess } = useContext(AccessContext);
-    const { uiConfig } = useUiConfig();
     const navigate = useNavigate();
 
     return (
@@ -47,26 +43,9 @@ export const ApiTokenPage = () => {
                 />
             }
         >
-            <Alert severity="info" className={styles.infoBoxContainer}>
-                <p>
-                    Read the{' '}
-                    <a
-                        href="https://docs.getunleash.io/docs"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Getting started guide
-                    </a>{' '}
-                    to learn how to connect to the Unleash API from your
-                    application or programmatically. Please note it can take up
-                    to 1 minute before a new API key is activated.
-                </p>
-                <br />
-                <strong>API URL: </strong>{' '}
-                <pre style={{ display: 'inline' }}>
-                    {uiConfig.unleashUrl}/api/
-                </pre>
-            </Alert>
+            <Box sx={{ mb: 4 }}>
+                <ApiTokenDocs />
+            </Box>
             <ConditionallyRender
                 condition={hasAccess(READ_API_TOKEN)}
                 show={() => <ApiTokenList />}
