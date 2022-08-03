3ba1bb64af22f764632519f8306a5df6f1104a74
sjaanus <sellinjaanus@gmail.com>
2022-08-03 16:52:26 +0000

    Refinement

--- src/component/admin/groups/CreateGroup/CreateGroup.tsx
+++ src/component/admin/groups/CreateGroup/CreateGroup.tsx
@@ -25,7 +25,6 @@ export const CreateGroup = () => {
         getGroupPayload,
         clearErrors,
         errors,
-        setErrors,
     } = useGroupForm();
 
     const { createGroup, loading } = useGroupApi();
--- src/component/admin/groups/hooks/useGroupForm.ts
+++ src/component/admin/groups/hooks/useGroupForm.ts
@@ -56,6 +56,5 @@ export const useGroupForm = (
         getGroupPayload,
         clearErrors,
         errors,
-        setErrors,
     };
 };
