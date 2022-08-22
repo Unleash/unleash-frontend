3bd3634570a8a18718b1b7a44c3f5a8b995a291d
olav <mail@olav.io>
2022-08-22 16:01:38 +0200

    fix: relax validation for required params

--- src/utils/validateParameterValue.ts
+++ src/utils/validateParameterValue.ts
@@ -4,20 +4,38 @@ import {
 } from 'interfaces/strategy';
 
 export const validateParameterValue = (
-    definition: IStrategyParameter,
+    definition: Pick<IStrategyParameter, 'type' | 'required'>,
     value: IFeatureStrategyParameters[string]
 ): string | undefined => {
     const { type, required } = definition;
 
-    if (required && value === '') {
+    if (shouldValidateRequired(type) && required && value === '') {
         return 'Field is required';
     }
 
-    if (type === 'number' && !isValidNumberOrEmpty(value)) {
+    if (shouldValidateNumeric(type) && !isValidNumberOrEmpty(value)) {
         return 'Not a valid number.';
     }
+
+    if (type === 'boolean' && !isValidBooleanOrEmpty(value)) {
+        return 'Not a valid boolean.';
+    }
+};
+
+// The components for booleans and percentages can't yet show error messages.
+// We should not enforce `required` until these errors can be shown in the UI.
+const shouldValidateRequired = (type: string): boolean => {
+    return type === 'string' || type === 'list' || type === 'number';
+};
+
+const shouldValidateNumeric = (type: string): boolean => {
+    return type === 'percentage' || type === 'number';
 };
 
 const isValidNumberOrEmpty = (value: string | number | undefined): boolean => {
     return value === '' || /^\d+$/.test(String(value));
+};
+
+const isValidBooleanOrEmpty = (value: string | number | undefined): boolean => {
+    return value === '' || value === 'true' || value === 'false';
 };
